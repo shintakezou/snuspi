01a16942d92a068b996ea37f928a963c4f886c76
shintakezou <shintakezou@users.sourceforge.net>
2017-12-17 13:34:36 +0100

    mods

--- snuspi.c
+++ snuspi.c
@@ -50,180 +50,186 @@ const char *trip_around_world = "wow! I always dreamed about a trip around the w
 
 int main(int argc, char **argv)
 {
-  int i, c;
-  char *filename = NULL, *pc;
-  FILE *srch;
-  bool ok = true;
+    int i, c;
+    char *filename = NULL, *pc;
+    FILE *srch;
+    bool ok = true;
   
-  for(i = 1; i < argc ; i++ )
-  {
-    if ( strcmp(argv[i], "snusp") == 0 )
+    for(i = 1; i < argc ; i++)
     {
-      if ( (i+1) < argc )
-      {
-	filename = argv[i + 1]; i++;
-      } else {
-	fprintf(stderr, "omitted string for 'snusp'\n");
-      }
-      continue;
+        if (strcmp(argv[i], "snusp") == 0)
+        {
+            if ((i+1) < argc)
+            {
+                filename = argv[i + 1]; i++;
+            } else {
+                fprintf(stderr, "omitted string for 'snusp'\n");
+            }
+            continue;
+        }
+        if (strcmp(argv[i], "debug") == 0)
+        {
+            debug = true;
+            continue;
+        }
+        fprintf(stderr, "unrecognized option '%s'\n", argv[i]);
     }
-    if ( strcmp(argv[i], "debug") == 0 )
-    {
-      debug = true; continue;
-    }
-    fprintf(stderr, "unrecognized option '%s'\n", argv[i]);
-  }
      
-  if ( filename == NULL )
-  {
-    fprintf(stderr, "no source file specified\n");
-    exit(1);
-  }
+    if (filename == NULL)
+    {
+        fprintf(stderr, "no source file specified\n");
+        exit(1);
+    }
   
-  /* load program */
-  if ( (srch = fopen(filename, "r") ) == NULL )
-  {
-    fprintf(stderr, "cannot locate file '%s'\n", filename);
-    exit(1);
-  }
-     
-  for (i = 0; (fgets(buffer, LINESIZE, srch) != NULL) && i < MAXHEIGHT; i++ )
-  {
-    field[i].s = strdup(buffer);
-    if ( field[i].s == NULL )
+    /* load program */
+    if ((srch = fopen(filename, "r") ) == NULL)
     {
-      fprintf(stderr, "can't allocate buffer\n");
-      exit(1);
+        fprintf(stderr, "cannot locate file '%s'\n", filename);
+        exit(1);
     }
-    field[i].l = strlen(buffer);
-    if ( (pc = strchr(buffer, snusp_START)) != NULL )
+     
+    for (i = 0; (fgets(buffer, LINESIZE, srch) != NULL) && i < MAXHEIGHT; i++)
     {
-      x = pc - buffer;
-      y = i;
+        field[i].s = strdup(buffer);
+        if (field[i].s == NULL)
+        {
+            fprintf(stderr, "can't allocate buffer\n");
+            exit(1);
+        }
+        field[i].l = strlen(buffer);
+        if ((pc = strchr(buffer, snusp_START)) != NULL)
+        {
+            x = pc - buffer;
+            y = i;
+        }
     }
-  }
-  if ( i == MAXHEIGHT )
-  {
-    fprintf(stderr, "bottom universal limit reached! Continuing anyway\n");
-  }
-  fclose(srch);
+    if (i == MAXHEIGHT)
+    {
+        fprintf(stderr, "bottom universal limit reached! Continuing anyway\n");
+    }
+    fclose(srch);
      
-  maxi = i;
+    maxi = i;
      
-  /* interpreter */
-  while( ok )
-  {
-    char cmd;
+    /* interpreter */
+    while (ok)
+    {
+        char cmd;
          
-    if ( (y >= i) || (y < 0) )
-    {
-      fprintf(stderr, in_the_space);
-      fprintf(stderr, "(%d,%d) (%d,%d)\n", x, y, dx, dy);
-      break;
+        if ((y >= i) || (y < 0))
+        {
+            fprintf(stderr, in_the_space);
+            fprintf(stderr, "(%d,%d) (%d,%d)\n", x, y, dx, dy);
+            break;
+        }
+        if ((x >= field[y].l) || (x < 0))
+        {
+            fprintf(stderr, in_the_space);
+            fprintf(stderr, "(%d,%d) (%d,%d)\n", x, y, dx, dy);
+            break;
+        }
+        cmd = field[y].s[x];
+        if (debug)
+        {
+            fprintf(stderr, "(%d, %d) (%d, %d) %c sp=%d idx=%d\n", x, y,
+                    dx, dy, cmd, sp, cellidx);
+        }
+        switch (cmd)
+        {
+        case snusp_LEAVE:
+            if (sp < MAXCALLS)
+            {
+                x = stack[sp].x;
+                y = stack[sp].y;
+                dx = stack[sp].dx;
+                dy = stack[sp].dy;
+                sp++;
+            } else {
+                ok = false;
+            }
+            break;
+        case snusp_ENTER:
+            sp--;
+            if (sp < 0)
+            {
+                fprintf(stderr, "too many nested calls! skipping!\n");
+                sp++;
+            } else {
+                stack[sp].x = x;
+                stack[sp].y = y;
+                stack[sp].dx = dx;
+                stack[sp].dy = dy;
+                x += dx; y += dy;
+            }
+            break;
+        case snusp_RIGHT:
+            if ((cellidx+1) >= MAXCELLS)
+            {
+                fprintf(stderr, trip_around_world);
+            }
+            cellidx = (cellidx+1)%MAXCELLS;
+            break;
+        case snusp_LEFT:
+            if ((cellidx-1) < 0)
+            {
+                fprintf(stderr, trip_around_world);
+            }
+            cellidx = (cellidx+MAXCELLS-1)%MAXCELLS;
+            break;
+        case snusp_INC:
+            cells[cellidx]++;
+            break;
+        case snusp_DEC:
+            cells[cellidx]--;
+            break;
+        case snusp_LURD:
+            /* \ */
+            if (dx != 0)
+            {
+                dy = dx;
+                dx = 0;
+            } else {
+                dx = dy;
+                dy = 0;
+            }
+            break;
+        case snusp_RULD:
+            /* / */
+            if (dx != 0)
+            {
+                dy = -dx;
+                dx = 0;
+            } else {
+                dx = -dy;
+                dy = 0;
+            }
+            break;
+        case snusp_SKIP:
+            x += dx;
+            y += dy;
+            break;
+        case snusp_SKIPZ:
+            if (cells[cellidx] == 0)
+            {
+                x += dx; y += dy;
+            }
+            break;
+        case snusp_READ:
+            c = getchar();
+            cells[cellidx] = c & 0xFF;
+            break;
+        case snusp_WRITE:
+            printf("%c", cells[cellidx]);
+            break;
+        default:
+            break;
+        }
+        x += dx;
+        y += dy;
     }
-    if ( (x >= field[y].l) || (x < 0) )
-    {
-      fprintf(stderr, in_the_space);
-      fprintf(stderr, "(%d,%d) (%d,%d)\n", x, y, dx, dy);
-      break;
-    }
-    cmd = field[y].s[x];
-    if ( debug )
-    {
-      fprintf(stderr, "(%d, %d) (%d, %d) %c sp=%d idx=%d\n", x, y,
-	      dx, dy, cmd, sp, cellidx);
-    }
-    switch ( cmd )
-    {
-    case snusp_LEAVE:
-      if ( sp < MAXCALLS )
-      {
-	x = stack[sp].x;
-	y = stack[sp].y;
-	dx = stack[sp].dx;
-	dy = stack[sp].dy;
-	sp++;
-      } else {
-	ok = false;
-      }
-      break;
-    case snusp_ENTER:
-      sp--;
-      if (sp < 0 )
-      {
-	fprintf(stderr, "too many nested calls! skipping!\n");
-	sp++;
-      } else {
-	stack[sp].x = x;
-	stack[sp].y = y;
-	stack[sp].dx = dx;
-	stack[sp].dy = dy;
-	x += dx; y += dy;
-      }
-      break;
-    case snusp_RIGHT:
-      if ( (cellidx+1) >= MAXCELLS )
-      {
-	fprintf(stderr, trip_around_world);
-      }
-      cellidx = (cellidx+1)%MAXCELLS;
-      break;
-    case snusp_LEFT:
-      if ( (cellidx-1) < 0 )
-      {
-	fprintf(stderr, trip_around_world);
-      }
-      cellidx = (cellidx+MAXCELLS-1)%MAXCELLS;
-      break;
-    case snusp_INC:
-      cells[cellidx]++;
-      break;
-    case snusp_DEC:
-      cells[cellidx]--;
-      break;
-    case snusp_LURD:
-      /* \ */
-      if ( dx != 0 )
-      {
-	dy = dx; dx = 0;
-      } else {
-	dx = dy;
-	dy = 0;
-      }
-      break;
-    case snusp_RULD:
-      /* / */
-      if ( dx != 0 )
-      {
-	dy = -dx; dx = 0;
-      } else {
-	dx = -dy; dy = 0;
-      }
-      break;
-    case snusp_SKIP:
-      x += dx; y += dy;
-      break;
-    case snusp_SKIPZ:
-      if ( cells[cellidx] == 0 )
-      {
-	x += dx; y += dy;
-      }
-      break;
-    case snusp_READ:
-      c = getchar();
-      cells[cellidx] = c & 0xFF;
-      break;
-    case snusp_WRITE:
-      printf("%c", cells[cellidx]);
-      break;
-    default:
-      break;
-    }
-    x += dx; y += dy;
-  }
      
-  for(i=0; i < maxi; i++)
-    free(field[i].s);
+    for(i=0; i < maxi; i++)
+        free(field[i].s);
 
-  return 0;
+    return 0;
 }
